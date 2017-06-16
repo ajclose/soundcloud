adec8309bb2183290e545626a8793c7ebfdf20c4
Alex Close <ajclose@gmail.com>
2017-06-16 15:22:00 -0500

    Add sort functionality

--- main.js
+++ main.js
@@ -25,8 +25,24 @@ form.addEventListener('submit', function(event) {
   fetchArtists(search)
 })
 
+function getReleaseDate(song) {
+  let date = String(song.release_year)
+  if (song.release_month < 10) {
+    date += "0"+String(song.release_month)
+  } else {
+    date += String(song.release_month)
+  }
+  if (song.release_day < 10) {
+    date+= "0"+String(song.release_day)
+  } else {
+    date += String(song.release_day)
+  }
+  return date
+}
+
 function fetchArtists(search) {
   document.querySelector('.results').textContent = ""
+  document.querySelector('.filter').value = "sort-by"
 fetch(`http://api.soundcloud.com/users/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=${search}`)
    .then( function(response){
      return response.json()
@@ -64,6 +80,7 @@ document.querySelector(".results").insertAdjacentHTML('beforeend', html)
           })
           .then(function(json){
             console.log(json);
+
             if (json.length > 0) {
 
 
@@ -87,6 +104,68 @@ document.querySelector(".results").insertAdjacentHTML('beforeend', html)
           } else {
             document.querySelector('.results').textContent = 'Uh oh!  This artist doesn\'t have any songs available for free.'
           }
+          const filter = document.querySelector('.filter')
+          filter.addEventListener('change', function() {
+            console.log("clicked!");
+            document.querySelector('.results').textContent = ''
+            const option = document.querySelector('.filter').value
+            console.log(option);
+            const sorted = json.sort(function(a, b) {
+              if (option === "title") {
+                var songA = a.title; // ignore upper and lowercase
+                var songB = b.title; // ignore upper and lowercase
+                if (songA < songB) {
+                  return -1;
+                }
+                if (songA > songB) {
+                  return 1;
+                }
+
+                // names must be equal
+                return 0;
+              } else if (option === "popularity") {
+                var songA = a.playback_count
+                var songB = b.playback_count
+                return parseInt(songB) - parseInt(songA)
+              } else if (option === "releaseDateOld") {
+                var songA = getReleaseDate(a)
+                var songB = getReleaseDate(b)
+                return parseInt(songA) - parseInt(songB)
+              } else if (option === "releaseDateNew") {
+                var songA = getReleaseDate(a)
+                var songB = getReleaseDate(b)
+                return parseInt(songB) - parseInt(songA)
+              }
+              console.log(sorted);
+
+            });
+
+            // const sorted = json.sort(function(a,b) {
+            //   return a.title - b.title
+            // })
+            console.log(sorted);
+            for (var k = 0; k < json.length; k++) {
+              const artwork = json[k].artwork_url
+              const title = json[k].title
+              const songSource = json[k].stream_url
+              const releaseDate = getReleaseDate(json[k])
+              const playbackCount = json[k].playback_count
+              const html = `
+              <div class="song" id="${songSource}">
+                <div class="artwork">
+                  <img src="${artwork}">
+                </div>
+                <div class="title">
+                  ${title}
+                  Plays: ${playbackCount}
+                  Release Date: ${releaseDate}
+                </div>
+              </div>
+              `
+
+              document.querySelector(".results").insertAdjacentHTML('beforeend', html)
+            }
+          })
         })
         .then(function() {
           const songs = document.querySelectorAll('.song')
@@ -99,10 +178,11 @@ document.querySelector(".results").insertAdjacentHTML('beforeend', html)
               document.querySelector(".song-title").textContent = song.querySelector('.title').textContent
               audio.src = song.id + "?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f";
               audio.autoplay = true
-            })
-          }
+
+              })
+            }
+          })
         })
-      })
-    }
-  })
-}
+      }
+    })
+  }
